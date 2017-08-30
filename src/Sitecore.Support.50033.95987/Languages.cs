6531efae8a54f3d919a54cb7e379a7649ee532c7
DK\obr <obr@sitecore.net>
2017-08-30 14:31:59 +0300

    Removed the adding of the flags

--- src/Sitecore.Support.50033.95987/Languages.cs
+++ src/Sitecore.Support.50033.95987/Languages.cs
@@ -1,9 +1,11 @@
 ﻿using Sitecore.Data.Items;
 using Sitecore.Diagnostics;
+using Sitecore.Globalization;
 using Sitecore.Shell.Framework.Commands;
 using Sitecore.Shell.Framework.Commands.Ribbon;
 using Sitecore.Support;
 using System;
+using System.Globalization;
 
 namespace Sitecore.Support.Shell.Framework.Commands.Ribbon
 {
@@ -11,20 +13,28 @@ namespace Sitecore.Support.Shell.Framework.Commands.Ribbon
     [Serializable]
     public class Languages : Sitecore.Shell.Framework.Commands.Ribbon.Languages
     {
-        public override string GetIcon(CommandContext context, string icon)
+        public override string GetHeader(CommandContext context, string header)
         {
             Assert.ArgumentNotNull(context, "context");
-            Assert.ArgumentNotNullOrEmpty(icon, "icon");
+            Assert.ArgumentNotNull(header, "header");
             if (context.Items.Length == 1)
             {
                 Item item = context.Items[0];
-                string str = ExtendedLanguageService.GetIcon(item.Language, item.Database);
-                if (!string.IsNullOrEmpty(str))
+                CultureInfo cultureInfo = item.Language.CultureInfo;
+                if (cultureInfo.IsNeutralCulture)
                 {
-                    return str;
+                    cultureInfo = Language.CreateSpecificCulture(cultureInfo.Name);
+                }
+                using (new ThreadCultureSwitcher(Context.Language.CultureInfo))
+                {
+                    string displayName = cultureInfo.DisplayName;
+                    if (!string.IsNullOrEmpty(displayName))
+                    {
+                        return displayName;
+                    }
                 }
             }
-            return base.GetIcon(context, icon);
+            return base.GetIcon(context, header);
         }
     }
 }
